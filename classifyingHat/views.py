7f7bf44ed3ee29df3506f28e5ae62d0c865f5a5f
Pedro Rey Anca <pedroreya@gmail.com>
2023-01-14 01:32:42 +0100

    Small refactor

--- classifyingHat/views.py
+++ classifyingHat/views.py
@@ -1,13 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, JsonResponse
 from django.urls import reverse
-from django.templatetags.static import static
 
 from .forms import ResultsIdForm
 
-from .utils.predict_house import predict_house
-from .utils.big_five_scraper import get_test_results
-from .utils.house_traits import all_house_traits
+from .utils.house_data import get_house_data
 
 
 def index(request):
@@ -17,22 +14,12 @@ def index(request):
 
 
 def results(request, results_id):
-    results = get_test_results(results_id)
-    house_name = predict_house(**results)
+    data = get_house_data(results_id)
 
-    house_logo = static(f'classifyingHat/house_logos/{house_name.lower()}.png')
-
-    house_traits = ', '.join(all_house_traits[house_name.lower()])
-
-    return render(request, 'classifyingHat/results.html', {'house_name': house_name, 'house_logo': house_logo, 'house_traits': house_traits})
+    return render(request, 'classifyingHat/results.html', data)
 
 def get_results(request):
     results_id = request.GET["results_id"]
-    results = get_test_results(results_id)
-    house_name = predict_house(**results)
+    data = get_house_data(results_id)
 
-    house_logo = static(f'classifyingHat/house_logos/{house_name.lower()}.png')
-
-    house_traits = ', '.join(all_house_traits[house_name.lower()])
-
-    return JsonResponse({'house_name': house_name, 'house_logo': house_logo, 'house_traits': house_traits})
+    return JsonResponse(data)
