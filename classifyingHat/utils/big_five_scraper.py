bb20b9b252d0b86b2fa50b802ab7610c0fc24c5c
Pedro Rey Anca <personal@peprolinbot.com>
2024-08-29 11:40:45 +0200

    Update scraper

--- classifyingHat/utils/big_five_scraper.py
+++ classifyingHat/utils/big_five_scraper.py
@@ -4,33 +4,36 @@ from bs4 import BeautifulSoup
 
 import re
 
+
 def get_test_results(results_id):
     if not re.match(r'^[a-zA-Z0-9]*$', results_id):
         raise TypeError("Invalid results ID")
-    results_url = f"https://bigfive-test.com/result/{results_id}?lang=en"
+    results_url = f"https://bigfive-test.com/en/result/{results_id}"
     req = requests.get(results_url)
 
-    soup = BeautifulSoup(req.content, 'html.parser') 
+    soup = BeautifulSoup(req.content, 'html.parser')
 
     domains = {
-        "extraversion" :"", 
-        "neuroticism" :"", 
-        "openness to experience" :"", 
-        "agreeableness" :"", 
-        "conscientiousness" :""
+        "extraversion": "",
+        "neuroticism": "",
+        "openness": "",
+        "agreeableness": "",
+        "conscientiousness": ""
     }
 
-    if soup.find('p', attrs={"class": "headline"}):
-        raise TypeError("Invalid results ID")
-
     try:
-        for key in domains.keys():
-            a = soup.find(id=key)
-            subheading = a.parent.find('p').contents[0]
-            domains[key] = int(re.findall(r'\d+', subheading)[0])
-    except:
-        raise Exception("There was an error while scraping the page")
+        hydration_scripts = ""
+        for script in soup.findAll('script'):
+            if script.string and "self.__next_f.push" in script.string:
+                hydration_scripts += script.string
 
-    domains['openness'] = domains.pop('openness to experience') # For cohesion with the rest of the modules
+        for key in domains.keys():
+            domain_dict = re.search(
+                r'{[^{]*\\"domain\\":\\"'+key[0].upper()+r'\\".*?}', hydration_scripts).group()  # The \ are because the json is escaped like that
+            domains[key] = int(re.search(
+                r'\\"score\\":(\d+),', domain_dict).group(1))  # I do this in a different search just in case the keys aren't in order (better safe than sorry)
+    except:
+        raise Exception(
+            "The id is invalid or there was an error while scraping the page")
 
     return domains
